09624ed99ee99f23a2a00f9d53d0f5a329c69984
Diego Sapriza <diego@sapriza.com>
2011-05-30 22:51:57 -0300

    init

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -3,6 +3,18 @@
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
 
-
+	/**
+	 * Starts Session
+	 */
+	protected function _initSession(){
+		Zend_Session::start();
+	}
+	
+    protected function _initConfig(){
+        $app_path = APPLICATION_PATH . '/config/application.ini';
+        $config = new Zend_Config_Ini($app_path, APPLICATION_ENV);
+        App_Singleton::setDefaultConfig($config);
+    }
+        
 }
 
