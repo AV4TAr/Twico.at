bc7cadf8906e236d7b1b8b0a23a9a08c44c06155
Diego Sapriza <diego@sapriza.com>
2011-05-30 23:16:31 -0300

    nuevo

--- public/index.php
+++ public/index.php
@@ -7,10 +7,12 @@ defined('APPLICATION_PATH')
 defined('APPLICATION_ENV')
     || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
 
+die(APPLICATION_PATH.'/models');
+
 // Ensure library/ is on include_path
 set_include_path(implode(PATH_SEPARATOR, array(
     realpath(APPLICATION_PATH . '/../library'),
-    realpath(APPLICATION_PATH.'./models'),
+    realpath(APPLICATION_PATH.'/models'),
     get_include_path(),
 )));
 
