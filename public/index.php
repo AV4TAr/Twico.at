b28610cf5188f12721a119bae60d43d08dbcab7a
Diego Sapriza <diego@sapriza.com>
2011-05-30 23:19:13 -0300

    .

--- public/index.php
+++ public/index.php
@@ -8,7 +8,8 @@ defined('APPLICATION_ENV')
     || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
 
 die(APPLICATION_PATH.'/models');
-
+// AVER
+// 
 // Ensure library/ is on include_path
 set_include_path(implode(PATH_SEPARATOR, array(
     realpath(APPLICATION_PATH . '/../library'),
