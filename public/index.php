a50a5d1959500a9d1d3807188ca447ff5db85730
Diego Sapriza <diego@sapriza.com>
2011-05-30 23:03:43 -0300

    models

--- public/index.php
+++ public/index.php
@@ -10,6 +10,7 @@ defined('APPLICATION_ENV')
 // Ensure library/ is on include_path
 set_include_path(implode(PATH_SEPARATOR, array(
     realpath(APPLICATION_PATH . '/../library'),
+    realpath(APPLICATION_PATH.'/models'),
     get_include_path(),
 )));
 
@@ -19,7 +20,7 @@ require_once 'Zend/Application.php';
 // Create application, bootstrap, and run
 $application = new Zend_Application(
     APPLICATION_ENV,
-    APPLICATION_PATH . '/configs/application.ini'
+    APPLICATION_PATH . '/config/application.ini'
 );
 $application->bootstrap()
             ->run();
